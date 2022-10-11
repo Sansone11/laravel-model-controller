bff61cba9e493b6d641b0068855cae0782260635
Sansone11 <sansonemirco@gmail.com>
2022-10-11 16:46:13 +0200

    env-modify

--- app/Http/Controllers/PageController.php
+++ app/Http/Controllers/PageController.php
@@ -2,11 +2,14 @@
 
 namespace App\Http\Controllers;
 
+use App\Movie;
 use Illuminate\Http\Request;
 
 class PageController extends Controller
 {
     public function index(){
+
+        $movies = Movie::all();
         return view('movie');
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -17,4 +17,4 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/','PageController@index');
+Route::get('/','PageController@index')->name('movie');
