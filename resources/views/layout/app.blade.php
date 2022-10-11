19b2db595a86c964a509a42edc5dddc77a550e25
Sansone11 <sansonemirco@gmail.com>
2022-10-11 22:52:28 +0200

    bug-fix

--- resources/views/layout/app.blade.php
+++ resources/views/layout/app.blade.php
@@ -5,17 +5,11 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>@yield('metaTitle')</title>
+    <link rel="stylesheet" href="{{asset('css/app.css')}}">
 </head>
 <body>
-    <header>
-        header
-    </header>
     <main>
         @yield('content')
-    </main>
-    <footer>
-        footer
-    </footer>
-    
+    </main>    
 </body>
 </html>
--- resources/views/movie.blade.php
+++ resources/views/movie.blade.php
@@ -5,15 +5,17 @@
 @section('content')
 
     <h1>Movie-list</h1>
-
+<div class="container">
     @foreach ($movies as $movie)
-    <div>
+   <div class="card_wrapper">
+    <div class="card">
         <h3>{{ $movie->title }}</h3>
         <p>{{ $movie->original_title }}</p>
         <p>{{ $movie->nationality}}</p>
         <p>{{ $movie->date}}</p>
         <p>{{ $movie->vote}}</p>
     </div> 
+   </div>
     @endforeach 
-        
+</div>       
 @endsection
